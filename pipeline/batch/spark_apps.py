72622f2e3fb56a74698f8ca6681c8dfe7c2d3877
Arturo Filastò <art@fuffa.org>
2015-06-04 19:17:15 +0200

    foo

--- pipeline/batch/spark_apps.py
+++ pipeline/batch/spark_apps.py
@@ -98,8 +98,7 @@ class InterestingToDB(luigi.postgres.CopyToTable):
     def rows(self):
         with self.input().open('r') as in_file:
             for line in in_file:
-                logging.info("Parsing %s" % line)
-                record = json_loads(unicode(line.strip('\n')))
+                record = json_loads(line.strip('\n').decode('utf-8', 'ignore'))
                 logger.info("Adding to DB %s" % (record["report_id"]))
                 yield self.serialize(record)
 
