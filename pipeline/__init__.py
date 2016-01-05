8d05d5a0263d9e459123a88eb79a26cf9586d38c
Arturo Filastò <accounts@fuffa.org>
2016-01-05 14:09:24 +0100

    Add all tasks to pipeline init

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,1 +1,14 @@
+from .batch.sanitise_reports import SanitiseBridgeReachability
+from .batch.anomaly_detection_heuristics import DetectAnomalousHTTPInvalidRequestLine
+from .batch.anomaly_detection_heuristics import DetectAnomalousHTTPHeaderFieldManipulation
+from .batch.anomaly_detection_heuristics import DetectAnomalousDNSConsistency
+from .batch.anomaly_detection_heuristics import DetectAnomalousHTTPRequestsMeasurements
 
+
+__all__ = [
+    "SanitiseBridgeReachability",
+    "DetectAnomalousHTTPInvalidRequestLine",
+    "DetectAnomalousHTTPHeaderFieldManipulation",
+    "DetectAnomalousDNSConsistency",
+    "DetectAnomalousHTTPRequestsMeasurements"
+]
