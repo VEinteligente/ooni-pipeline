9e17ff12c33b14440eadec61a8e52f2f4654caff
Arturo Filastò <accounts@fuffa.org>
2016-01-10 23:02:20 +0100

    Add module to contructor

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,18 +1,16 @@
-from .batch.sanitise_reports import SanitiseBridgeReachability
 from .batch.anomaly_detection_heuristics import DetectAnomalousHTTPInvalidRequestLine
 from .batch.anomaly_detection_heuristics import DetectAnomalousHTTPHeaderFieldManipulation
 from .batch.anomaly_detection_heuristics import DetectAnomalousDNSConsistency
 from .batch.anomaly_detection_heuristics import DetectAnomalousHTTPRequests
 from .batch.anomaly_detection_heuristics import DetectAllAnomalies
-from .batch.publish_reports import PublishReports
+from .batch.publish_sanitise_reports import PublishReportsAndStreamsRange
 
 
 __all__ = [
-    "SanitiseBridgeReachability",
     "DetectAnomalousHTTPInvalidRequestLine",
     "DetectAnomalousHTTPHeaderFieldManipulation",
     "DetectAnomalousDNSConsistency",
     "DetectAnomalousHTTPRequests",
-    "PublishReports",
+    "PublishReportsAndStreamsRange",
     "DetectAllAnomalies"
 ]
