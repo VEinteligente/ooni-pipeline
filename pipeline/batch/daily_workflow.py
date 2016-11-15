ba3cbd16db3843156e79e8e140eb9ba87f5d4033
Andres A <andresazp@gmail.com>
2016-11-15 17:59:13 -0400

    Auto stash for revert of "se creo el task VerifyFlags sin requisitos y solo da hello world"

--- pipeline/batch/daily_workflow.py
+++ pipeline/batch/daily_workflow.py
@@ -838,6 +838,20 @@ class InsertMeasurementsIntoPostgres(luigi.postgres.CopyToTable):
                 yield self._format_record(line.strip(), idx)
 
 
+class VerifyFlags(luigi.Task):
+    report_path = luigi.Parameter()
+
+    def requires(self):
+        return None
+
+    def output(self):
+        return luigi.LocalTarget('helloworld.txt')
+
+    def run(self):
+        print "Hello!"
+        with self.output().open('w') as outfile:
+            outfile.write('Hello World!\n')
+
 class UpdateView(RunQuery):
     # This is needed so that it gets re-run on new intervals
     date_interval = luigi.DateIntervalParameter()
